2dc3379cc0ae26515ea98013f758fcedcd6cbca8
JPEtienne <ji.pe.7@hotmail.com>
2021-12-11 23:19:48 +0100

    Working on SendMail component

--- src/components/SendMail/SendMail.tsx
+++ src/components/SendMail/SendMail.tsx
@@ -1,10 +1,36 @@
+import CloseIcon from '@mui/icons-material/Close'
+import { Button } from '@mui/material'
 import { FC } from 'react'
 import './SendMail.css'
 
 interface ISendMailProps {}
 
-const SendMail: FC<ISendMailProps> = (props) => {
-  return <div className="send-mail"></div>
+const SendMail: FC<ISendMailProps> = () => {
+  return (
+    <div className="send-mail">
+      <div className="send-mail__header">
+        <h3>New Message</h3>
+        <CloseIcon className="send-mail__close" />
+      </div>
+
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+
+        <div className="send-mail__options">
+          <Button
+            className="send-mail__send"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send
+          </Button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default SendMail
