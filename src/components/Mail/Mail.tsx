e2d6c8d34cc0afe6ade3e9d0e952eac661dd76cd
JPEtienne <ji.pe.7@hotmail.com>
2021-12-11 22:18:00 +0100

    Mail component

--- src/components/Mail/Mail.tsx
+++ src/components/Mail/Mail.tsx
@@ -1,10 +1,81 @@
-import * as React from 'react';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant'
+import MoveToInboxIcon from '@mui/icons-material/MoveToInbox'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+import WatchLaterIcon from '@mui/icons-material/WatchLater'
+import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import DeleteIcon from '@mui/icons-material/Delete'
+import PrintIcon from '@mui/icons-material/Print'
+import ErrorIcon from '@mui/icons-material/Error'
+import EmailIcon from '@mui/icons-material/Email'
+import { useHistory } from 'react-router-dom'
+import { IconButton } from '@mui/material'
+import { FC } from 'react'
+import './Mail.css'
 
-interface IMailProps {
+interface IMailProps {}
+
+const Mail: FC<IMailProps> = () => {
+  const history = useHistory()
+  return (
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__tools-left">
+          <IconButton onClick={() => history.push('/')}>
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail__tools-right">
+          <IconButton>
+            <UnfoldMoreIcon />
+          </IconButton>
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+          <IconButton>
+            <ExitToAppIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="mail__body">
+        <div className="mail__body-header">
+          <h2>Subject</h2>
+          <LabelImportantIcon className="mail__important" />
+          <p>Title</p>
+          <p className="mail__time">11:13am</p>
+        </div>
+        <div className="mail__message">
+          <p>This is a message</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-const Mail: React.FunctionComponent<IMailProps> = (props) => {
-  return <div className="mail"></div>
-};
-
-export default Mail;
+export default Mail
